97f038a51dfabd7abe6895d7178c19fa238080fb
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-07-24 04:14:34 +0300

    basic caching class

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Basic Caching system module.
+"""
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache class that inherits from BaseCaching and is a caching system.
+    """
+
+    def __init__(self):
+        """
+        Initialize the cache using the parent class initializer.
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add an item to the cache.
+
+        Parameters:
+        key (str): The key for the cache item.
+        item (any): The item to cache.
+
+        If key or item is None, the method does nothing.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieve an item from the cache.
+
+        Parameters:
+        key (str): The key for the cache item.
+
+        Returns:
+        any: The cached item, or None if key is None or key doesn't exist.
+        """
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
