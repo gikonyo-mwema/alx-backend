10c86389a45606fe071063a6077c8a78142394d8
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-07-27 04:07:55 +0300

    hypermedia

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """
-Checks if input arguments are out of range
+Function to calculate the start and end indices for pagination.
 """
+
 import csv
+from typing import List, Tuple, Dict
 import math
-from typing import List
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Returns a tuple of size two - start and end index corresponding
     to the range to return in a list for those pagination parameters
@@ -45,28 +46,48 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        Returns a page of the datases.
+        Get a page of the dataset.
 
         Parameters:
         - page (int): The current page number (1-indexed).
         - page_size (int): The number of items per page.
 
         Returns:
-        - List[List]: A list of lists containing the row of the dataset
+        - List[List]: A list of lists containing
+        the rows for the specified page.
         """
-        assert isinstance(page, int) and page > 0,
-        "Page must be a positive integer"
-        assert isinstance(page_size, int) and page_size > 0,
-        "Page size must be a positive integer"
-
-        self.dataset()  # Ensure dataset is loaded
-
+        assert type(page) == int and type(page_size) == int
+        assert (page > 0 and page_size > 0)
         start_index, end_index = index_range(page, page_size)
-        dataset = self.__dataset
+        dataset = self.dataset()
 
-        if start_index >= len(dataset):
+        if start_index > len(dataset):
             return []
 
         return dataset[start_index:end_index]
 
-    def get_hyper
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """
+        Get a page of the dataset with hypermedia information.
+
+        Parameters:
+        - page (int): The current page number (1-indexed).
+        - page_size (int): The number of items per page.
+
+        Returns:
+        - Dict: A dictionary containing pagination information and the data.
+        """
+        data = self.get_page(page, page_size)
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
+
+        hyper = {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total_pages
+        }
+
+        return hyper
